a1f18dfc6af47ac77adbc41ae8b54e37e13e9258
chloe0x0 <ChloeZeiter@gmail.com>
2022-10-22 13:41:55 -0700

    Format

--- src/E.c
+++ src/E.c
@@ -7,10 +7,16 @@
 // Usage string
 #define USAGE "Usage: E <cells> <rule> <iterations> <init_state, 1 or 0>\nIf init state is 1, only the center cell is alive at start, otherwise cells are alive at random"
 
-struct Universe{ bool* state; uint8_t rule; size_t length; };
+struct Universe
+{
+    bool *state;
+    uint8_t rule;
+    size_t length;
+};
 
-struct Universe* ConstructUniverse(size_t len, uint8_t rule){
-    struct Universe* u = malloc( sizeof(struct Universe) );
+struct Universe *ConstructUniverse(size_t len, uint8_t rule)
+{
+    struct Universe *u = malloc(sizeof(struct Universe));
     u->state = calloc(len, sizeof(bool));
     u->rule = rule;
     u->length = len;
@@ -19,55 +25,67 @@ struct Universe* ConstructUniverse(size_t len, uint8_t rule){
 }
 
 // Function to free a Universe struct from memory
-inline void FreeUniverse(struct Universe* universe){
+inline void FreeUniverse(struct Universe *universe)
+{
     free(universe->state);
     free(universe);
 }
 
 // Randomly sets the cell states
-inline void RandomState(struct Universe* u){
-    for (size_t i = 0; i < u->length; ++i){
+inline void RandomState(struct Universe *u)
+{
+    for (size_t i = 0; i < u->length; ++i)
+    {
         u->state[i] = rand() % 2;
     }
 }
 
 // Sets only the center cell to true
-inline void CenterState(struct Universe* u){
+inline void CenterState(struct Universe *u)
+{
     u->state[u->length / 2] = 1;
 }
 
 // evolve the state of the system by one epoch according to its rule
-void Evolve(struct Universe* universe){
+void Evolve(struct Universe *universe)
+{
     int ruleIndex;
-    bool* newState = calloc(universe->length, sizeof(bool));
+    bool *newState = calloc(universe->length, sizeof(bool));
 
-    for (size_t i = 0; i <= universe->length; ++i){
+    for (size_t i = 0; i <= universe->length; ++i)
+    {
         bool l, c, r;
         l = i ? universe->state[i - 1] : universe->state[universe->length];
         c = universe->state[i];
         r = universe->state[i + 1];
 
-        ruleIndex = (l*4) + (c*2) + (r);        
+        ruleIndex = (l * 4) + (c * 2) + (r);
         newState[i] = universe->rule & (1 << ruleIndex);
     }
-    
+
     free(universe->state);
     universe->state = newState;
 }
 
-char* Universe2String(struct Universe* universe, bool displayEmpty){
-    char* str = malloc( sizeof(char) * universe->length+1 );
-    const char empty = displayEmpty ? '0' : ' '; 
+char *Universe2String(struct Universe *universe, bool displayEmpty)
+{
+    char *str = malloc(sizeof(char) * universe->length + 1);
+    const char empty = displayEmpty ? '0' : ' ';
 
-    for (size_t i = 0; i <= universe->length; ++i){ str[i] = universe->state[i] ? '1' : empty; }
+    for (size_t i = 0; i <= universe->length; ++i)
+    {
+        str[i] = universe->state[i] ? '1' : empty;
+    }
     str[universe->length + 1] = '\0';
 
     return str;
 }
 
 // E.exe <Length> <Rule> <Epochs> <Centered>
-int main(int argc, char* argv[]){
-    if (argc < 5){
+int main(int argc, char *argv[])
+{
+    if (argc < 5)
+    {
         fprintf(stderr, "Insufficient flags provided!\n");
         fprintf(stderr, USAGE);
         exit(1);
@@ -80,13 +98,20 @@ int main(int argc, char* argv[]){
 
     srand(time(NULL));
 
-    struct Universe* u = ConstructUniverse(Length, Rule);
+    struct Universe *u = ConstructUniverse(Length, Rule);
 
-    if (centered){ CenterState(u); }
-    else { RandomState(u); }
+    if (centered)
+    {
+        CenterState(u);
+    }
+    else
+    {
+        RandomState(u);
+    }
 
     printf("%s\n", Universe2String(u, false));
-    for (size_t i = 0; i < Epochs; ++i){
+    for (size_t i = 0; i < Epochs; ++i)
+    {
         Evolve(u);
         printf("%s\n", Universe2String(u, false));
     }
